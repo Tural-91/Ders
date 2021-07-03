8cc9eccc368fb68b22d02dd842fef5332f1b7ecb
Tural <tural.777.aqayev@gmail.com>
2021-07-03 22:32:39 +0400

    update

--- Algorithms/hw2/app.js
+++ Algorithms/hw2/app.js
@@ -1,12 +1,11 @@
-let mainElement = document.createElement('div');
 
+
+let mainElement = document.createElement('div')
 mainElement.classList.add('block');
-
 document.querySelector('body').appendChild(mainElement);
 
 for (let i = 0; i < 10; i++) {
     let element = document.createElement('div');
+    document.querySelector('div').appendChild(element);
     element.classList.add('miniBlock');
 }
-
-console.log(mainElement);
