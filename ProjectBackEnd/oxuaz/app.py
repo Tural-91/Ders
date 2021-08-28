566059a4b07485aed1c585311e0ab644e28cfa12
Tural <tural.777.aqayev@gmail.com>
2021-08-28 05:16:57 +0400

    tapsiriq

--- ProjectBackEnd/oxuaz/app.py
+++ ProjectBackEnd/oxuaz/app.py
@@ -1,26 +1,10 @@
+import re
 from flask import Flask,render_template,redirect,request,url_for
 
-xeber=[
-{       'id':1,
-        'title':'“Neftçi” avrokuboklarla böyük hesablı məğlubiyyətlə vidalaşdı',
-        'desc':'İsraildə keçirilən “Makkabi” (Hayfa) – “Neftçi” görüşü meydan sahibi komandanın 4:0 hesablı darmadağını ilə başa çatıb.',
-        'content':'İsraildə keçirilən “Makkabi” (Hayfa) – “Neftçi” görüşü meydan sahibi komandanın 4:0 hesablı darmadağını ilə başa çatıb. Bununla da Avropa Konfrans Liqasının qrup mərhələsinə yəhudilər yollanib. “Neftçi” isə cari mövsüm avrokubok macərasını başa vurub.'
+xeber1=[
 
-     },
-{       'id':2,
-        'title':'Kabildə yeni partlayışlar: Ölən ABŞ hərbçilərinin sayı artıb ',
-        'desc':'Kabildə daha iki partlayış baş verib.',
-        'content':'Bu barədə “Al Arabiya” məlumat yayıb. Məlumata əsasən, partlayış səsləri paytaxtın kənar məhəllərində eşidilib.Pentaqondan verilən məlumata görə, terror aktları nəticəsində 12 ABŞ hərbçisi ölüb.Onlardan 11-i dəniz piyadası, biri isə hərbi həkimdir.'
-
-     },
-{  
-        'id':3,
-        'title':'Bayden İsrail Baş naziri ilə görüşünü, Merkel isə İsrailə səfərini təxirə saldı',
-        'desc':'Co Bayden ölkədə səfərdə olan İsrailin Baş naziri Naftali Bennetlə görüşünü təxirə salıb.',
-        'content':'Kabil hava limanı yaxınlığında baş vermiş terror aktından sonra ABŞ prezidenti Co Bayden ölkədə səfərdə olan İsrailin Baş naziri Naftali Bennetlə görüşünü təxirə salıb.Bu barədə “Axios” məlumat yayıb. Almaniya kansleri Angela Merkel isə Əfqanıstandakı vəziyyətlə əlaqədar avqustun 28-nə nəzərdə tutulmuş İsrailə səfərini ləğv edib.'
-
-     },
 ]
+id=1
 
 app=Flask(__name__)
 
@@ -34,14 +18,41 @@ def about():
 
 @app.route('/xeber')
 def posts():
-    return render_template('xeber.html',xeber=xeber)
+    return render_template('xeber.html',xeber=xeber1)
 
 @app.route('/single/<id>')
 def single_post(id):
-    for news in xeber:
+    for news in xeber1:
         if news['id']==int(id):
             return render_template('single.html',single=news)
     return redirect('/xeber')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')   
+
+@app.route('/admin/add',methods=['GET','POST'])
+def add():
+    if request.method=='POST':
+        global id
+        basliq=request.form['title']
+        xulase=request.form['desc']
+        metn=request.form['content']
+     
+        xeber={
+             'id':id,
+             'title':basliq,
+             'desc':xulase,
+             'content':metn
+        }
+
+        xeber1.append(xeber)
+        id=id+1
+        return redirect('/xeber')
+    
+    return render_template('add.html')
+
+   
 
 if __name__=='__main__':
     app.run(debug=True)
